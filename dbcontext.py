21e3928180d0cc94fac58d528f3bf257e6722bd9
tehila6754 <teila6754@gmail.com>
2025-06-29 15:36:58 +0300

    Update dbcontext.py

--- dbcontext.py
+++ dbcontext.py
@@ -18,13 +18,21 @@ config = {
     "port": 3306
 }
 
+def demo_data() -> List[Person]:
+
+    person1 = Person(1, "John", "Doe", 30, "76 Ninth Avenue St, New York, NY 10011, USA", "Google")
+    person2 = Person(2, "Jane", "Doe", 28, "15 Aabogade St, Aarhus, Denmark 8200", "Microsoft")
+    person3 = Person(3, "Jack", "Doe", 25, "98 Yigal Alon St, Tel Aviv, Israel 6789141", "Amazon")
+
+    return [person1, person2, person3]
+
 def db_data() -> List[Person]:
     if not db_host:
-        return demo_data()  # פונקציה שאתה מגדיר במקרה שאין חיבור למסד נתונים.
-
+        return demo_data()
+    
     if not (db_user and db_pass):
         raise Exception("DB_USER and DB_PASS are not set")
-
+    
     cnx = mysql.connector.connect(**config)
     result = []
     if cnx.is_connected():
@@ -42,7 +50,6 @@ def db_data() -> List[Person]:
 def db_delete(id: int) -> Response:
     if not db_host:
         return Response(status=200)
-    
     cnx = mysql.connector.connect(**config)
     status = 200
     if cnx.is_connected():
@@ -50,9 +57,8 @@ def db_delete(id: int) -> Response:
         try:
             cursor.execute(f"DELETE FROM people WHERE id = {id}")
             cnx.commit()
-        except Exception as e:
+        except:
             status = 404
-            print(f"Error deleting: {e}")
         finally:
             if cnx.is_connected():
                 cursor.close()
@@ -62,7 +68,6 @@ def db_delete(id: int) -> Response:
 def db_add(person: Person) -> Response:
     if not db_host:
         return Response(status=200)
-    
     cnx = mysql.connector.connect(**config)
     status = 200
     personId = 0
@@ -72,19 +77,18 @@ def db_add(person: Person) -> Response:
             cursor.execute(f"INSERT INTO people (firstName, lastName, age, address, workplace) VALUES ('{person.first_name}', '{person.last_name}', {person.age}, '{person.address}', '{person.workplace}')")
             cnx.commit()
             personId = cursor.lastrowid
-        except Exception as e:
+        except:
             status = 404
-            print(f"Error adding: {e}")
         finally:
             if cnx.is_connected():
                 cursor.close()
                 cnx.close()
     return Response(status=status, response=str(personId))
 
+
 def health_check() -> bool:
     if not db_host:
         return True
-    
     cnx = mysql.connector.connect(**config)
     response = False
     if cnx.is_connected():
@@ -98,4 +102,3 @@ def health_check() -> bool:
                 cursor.close()
                 cnx.close()
     return response
-
